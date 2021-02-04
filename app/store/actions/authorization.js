0c1b6f0cffd6ad7ee3ea8790d33e4f6c345fd1c8
Adem Bacaj <adem@notiphy.io>
2021-02-05 00:51:42 +0100

    config file added

--- app/store/actions/authorization.js
+++ app/store/actions/authorization.js
@@ -2,9 +2,9 @@ import { SET_IS_LOGGED_IN, SET_ACCOUNT_TYPE } from '../actionTypes';
 
 export const setIsLoggedIn = (payload) => (dispatch) => {
     dispatch({ type: SET_IS_LOGGED_IN, payload });
-    if (payload?.email.includes('business')) {
-        dispatch({ type: SET_ACCOUNT_TYPE, payload: 1 })
-    } else {
-        dispatch({ type: SET_ACCOUNT_TYPE, payload: 0 })
-    }
+    // if (payload?.email.includes('business')) {
+    //     dispatch({ type: SET_ACCOUNT_TYPE, payload: 1 })
+    // } else {
+    //     dispatch({ type: SET_ACCOUNT_TYPE, payload: 0 })
+    // }
 }
--- index.js
+++ index.js
@@ -10,6 +10,10 @@ import { name as appName } from './app.json';
 import { enableScreens } from 'react-native-screens';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import axios from 'axios';
+import { BASE_URL } from './app/common/config';
+
+axios.defaults.baseURL = BASE_URL;
 
 enableScreens();
 
